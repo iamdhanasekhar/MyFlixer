31338bbb85a01f6e88fe341957bfc7670502853c
Eresu Dhana sekhar reddy <133117532+iamdhanasekhar@users.noreply.github.com>
2025-08-05 09:28:34 +0530

    Update MovieDetails.js
    resolving insecure resource error on getting data from api.

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -41,7 +41,7 @@ export function MovieDetails({
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${key}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${key}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
